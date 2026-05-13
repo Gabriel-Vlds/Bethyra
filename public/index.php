c6bcee141d6c339dfd407456f84adb43f655532c
Gabriel Valdes <isidrogvaldes@gmail.com>
2026-05-13 15:55:31 -0600

    13-may-2026 Maquetacion inicial
    Se inicio la maquetacion inicial con html en index.php

---
+++ public/index.php
@@ -0,0 +1,53 @@
+<!Doctype html>
+<html lang="es">
+<head>
+    <meta charset ="UTF-8">
+    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bethyra</title>
+</head>
+
+<body>
+    <header>
+        <nav class="menu_principal">
+            <ul>
+                <li class="menu_item"><a href="">Inicio</a></li>
+                <li class= "menu_item"><a href="">Servicios</a></li>
+                <li class= "menu_item"><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <main>
+        <section class="Hero">
+            <div>
+                <h2>El internet y la tecnologia trabajando para ti, no al reves</h2>
+                <p>En Bethyra construimos paginas web profesionales y agentes especializados en automatizar atencion al cliente, ventas y operaciones de tu empresa.</p>
+                <button id="boton_llamada">Dinos como ayudarte</button>
+                <button id="boton_VerServicios">Servicios</button>
+            </div>
+        </section>
+
+        <section class="sobre_nosotros">
+            <h2>Desrrollo a la medida e inteligente</h2>
+            <p>Bethyra nace de la creciente necesidad tecnologica para los pequeños medianos y grandes empresarios que necesitan impulsar su negocio con la ayuda de las tecnologias mas nuevas en desarrollo web e inteligencia artificial, nosotros proponemos soluciones que se adaptan a tus problemas no al reves.</p>
+        </section>
+        
+        <section class="servicios">
+            <div>
+                <h2>Desarrollo Web</h2>
+                <h4>Landing pages y sitios corporativos</h4>
+                <p>Diseño moderno,rapido,optimizado y responsivo para cualquier navegador</p>
+                <h4>Tiendas en linea</h4>
+                <p>Catalogo completo,pasarelas de pago nacionales e internacionales,envios automatizados y panel de administracion para que no necesites un equipo manejando tu tienda.</p>
+                <h4>APIs y sistemas a medida</h4>
+                <p>Esa plataforma especifica que sabes que va a impulsarte, nosotros la hacemos para que te sirva a ti no para que te esclavice con problemas.</p>
+                <h4>Mantenimiento</h4>
+                <p>Actualizaciones,respaldos,seguridady mejoras continuas para que tu sitio no se caiga ni se quede atras.</p>
+            </div>
+        </section>
+
+    
+    </main>
+
+</body>
