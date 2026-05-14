78b1b1e903a42907295ae611a2b73ff1872dcce7
Gabriel Valdes <isidrogvaldes@gmail.com>
2026-05-14 08:11:15 -0600

    Avance maquetacion 14-may-2026

--- public/index.php
+++ public/index.php
@@ -33,18 +33,42 @@
             <p>Bethyra nace de la creciente necesidad tecnologica para los pequeños medianos y grandes empresarios que necesitan impulsar su negocio con la ayuda de las tecnologias mas nuevas en desarrollo web e inteligencia artificial, nosotros proponemos soluciones que se adaptan a tus problemas no al reves.</p>
         </section>
         
-        <section class="servicios">
-            <div>
+        <section class="servicios_desarrollo">
+            <div class="servicios">
                 <h2>Desarrollo Web</h2>
                 <h4>Landing pages y sitios corporativos</h4>
                 <p>Diseño moderno,rapido,optimizado y responsivo para cualquier navegador</p>
+            </div>
+            <div class="servicios">   
                 <h4>Tiendas en linea</h4>
                 <p>Catalogo completo,pasarelas de pago nacionales e internacionales,envios automatizados y panel de administracion para que no necesites un equipo manejando tu tienda.</p>
+            </div> 
+            <div class="servicios">  
                 <h4>APIs y sistemas a medida</h4>
                 <p>Esa plataforma especifica que sabes que va a impulsarte, nosotros la hacemos para que te sirva a ti no para que te esclavice con problemas.</p>
+            </div> 
+            <div class="servicios">  
                 <h4>Mantenimiento</h4>
                 <p>Actualizaciones,respaldos,seguridady mejoras continuas para que tu sitio no se caiga ni se quede atras.</p>
             </div>
+        </section>
+        <section class="servicios_agentes">
+            <div>
+            <h2>Empleados IA</h2>
+            <p>Construimos agentes de IA conectados a tus procesos, no es un simple chatbot con respuestas enlatadas,lo entrenamos en base a tus necesidades y parametros.</p>
+         </div>
+         <div>
+            <h4>Atencion al cliente automatizada</h4>
+            <p>Olvidate de estar respondiendo por horas mensajes que no llegan a nada, con nuestros agentes tienes respuesta 24/7 en Whatsapp, Chatbot o correo.</p>
+        </div>
+        <div>
+            <h4>Agente de ventas</h4>
+            <p>El agente de ventas califica leads, agenda citas y dan seguimiento para que no pierdas tu tiempo.</p>
+        </div>
+        <div>    
+            <h4>Asistentes internos</h4>
+            <p>El asistente es el cerebro logistico que te ayuda a tomar decisiones,genera reportes de tu negocio, y te ayuda a tomar decisiones basadas en tu negocio.</p>    
+        </div>
         </section>
 
     
